88c82534ea14eb688190c2a3a8883244b276d782
Brad Crittenden <bac@canonical.com>
2014-02-27 09:04:54 -0400

    Update version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 setup(
     name='charmworldlib',
-    version="0.2.4-1",
+    version="0.2.4-2",
     packages=['charmworldlib'],
     maintainer='Marco Ceppi',
     maintainer_email='marco@ceppi.net',
