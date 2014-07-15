e6aa57d55414128abfec10fa966c73a1359a16f4
Marco Ceppi <marco@ceppi.net>
2014-07-15 13:52:36 -0400

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='charmworldlib',
-    version="0.4.0",
+    version="0.4.1",
     packages=['charmworldlib'],
     maintainer='Marco Ceppi',
     maintainer_email='marco@ceppi.net',
