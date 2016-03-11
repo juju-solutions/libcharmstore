70fb60069667ffea978bd7ecf823b9a78e28ef51
Marco Ceppi <marco@ceppi.net>
2016-03-11 13:09:28 -0500

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='libcharmstore',
-    version="0.0.1",
+    version="0.0.2",
     packages=['charmstore'],
     maintainer='Marco Ceppi',
     install_requires=[
